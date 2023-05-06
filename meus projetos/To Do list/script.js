584cb29e3b01b9d18bff237c4d68dfd5f591a9de
Nickangelo01 <nicolasa.smacedo@gmail.com>
2023-05-06 11:55:09 -0300

    ta indo

---
+++ meus projetos/To Do list/script.js
@@ -0,0 +1,13 @@
+//constantes e seletores
+const area= document.querySelector(".area textarea"),
+todo=document.querySelector(".todoLists"),
+numPendente= document.querySelector(".pendenteNum"),
+limpar=document.querySelector(".limpar");
+
+//vamos fazer a função para adicionar, limpar e confirmar a tarefa
+
+function{
+    let task = document.querySelectorAll(".pendente");
+    numPendente.textContent = task.length === 0 ? "no" : task.length;
+    let allList = 
+}
