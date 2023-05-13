5f03d43e56a3da3c3cbc9acc2b8ace5bd3c78d9e
Nickangelo01 <nicolasa.smacedo@gmail.com>
2023-05-12 23:29:01 -0300

    ja me perdi nesse projeto

--- meus projetos/To Do list/script.js
+++ meus projetos/To Do list/script.js
@@ -1,13 +1,62 @@
-//constantes e seletores
-const area= document.querySelector(".area textarea"),
-todo=document.querySelector(".todoLists"),
-numPendente= document.querySelector(".pendenteNum"),
-limpar=document.querySelector(".limpar");
+//add variavel
+const form = document.getElementById('form')
+input_field =document.getElementById('input-tarefas')
+toDos = document.getElementById('toDos')
+clear_button = document.getElementById('limpar')
+pendingNum = document.getElementById('pending-num')
 
-//vamos fazer a função para adicionar, limpar e confirmar a tarefa
+const todos = JSON.parse(local.Storage.getItem('toDos'))
 
-function{
-    let task = document.querySelectorAll(".pendente");
-    numPendente.textContent = task.length === 0 ? "no" : task.length;
-    let allList = 
+if (todos){
+    todos.forEach( todos => addTodo(todo))
+}
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    addTodo()
+
+})
+
+function addTodo(todo){
+    let todoText = input.value
+
+    if(todo){
+        todoText = todo.text
+    }
+
+    if(todoText){
+        const todoEl = document.createElement('li')
+        if(todo && todo.completed){
+            todoEl.classList.add('completed')
+        }
+        todoEl.innerText = todoText
+
+        todoEl.addEventListener('contextmenu', (e) =>{
+            e.preventDefault()
+
+            todoEl.remove()
+            updateLS()
+        })
+        toDos.appendChild(todoEl)
+
+        input.value = ''
+
+        updateLS()
+    }
+}
+
+function updateLS(){
+    todosEl = document.querySelectorAll('li')
+
+    const todos = []
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text : todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+    
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
